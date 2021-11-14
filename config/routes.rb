6a4664d6d9dbae8163a11b576dc261f88b67cf08
benoit leboucher <ben.lebou@gmail.com>
2021-11-14 14:06:23 -0500

    Bookmark part created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   root to: 'lists#index'
   resources :lists, except: [:edit, :update]
   # resources :lists, except [:edit, :update]
